fefbdcb2177f02f72a02ebd8597105cdb5ac93a7
AayushXmusic <142298501+AayushXmusic@users.noreply.github.com>
2023-09-08 08:00:33 +0530

    Update bot.py

--- AnonX/core/bot.py
+++ AnonX/core/bot.py
@@ -12,7 +12,7 @@ class AnonXBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            "AlishaXMusic",
+            "❛- 𝙅𝘼𝙎𝙄𝘾𝘼 мυѕι¢ вσт",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
